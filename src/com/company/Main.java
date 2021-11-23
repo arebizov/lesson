a1da979124d6108c3510ecdb6be4b364ff4a51e6
Anatoliy <arebizov@yandex.ru>
2021-11-23 23:57:07 +0300

    change logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,15 +12,23 @@ public class Main {
 
         String expressionString1 = "";
         String expressionString2 = "";
+        //ArrayList<Example> cats = new ArrayList<Example>();
+        //ArrayList<String> arrayList= new ArrayList();
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         StringBuilder str = new StringBuilder();
-        int t =0;
-        for (int i = 0; i < 5; i++) {
-        int a = ThreadLocalRandom.current().nextInt(1, 25);
-        int x = ThreadLocalRandom.current().nextInt(1, 25);
-        int b = ThreadLocalRandom.current().nextInt(1, 25);
-
-        int c;
+        System.out.println("Добрый день, введите своё имя:");
+        String name = scanner.nextLine();
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+        int t = 0;
+        String s = "";
+        System.out.println(s);
+        while (!s.equals("q")) {
+            int a = ThreadLocalRandom.current().nextInt(1, 25);
+            int x = ThreadLocalRandom.current().nextInt(1, 25);
+            int b = ThreadLocalRandom.current().nextInt(1, 25);
+            int c;
 
             int expression = ThreadLocalRandom.current().nextInt(0, 5);
             switch (expression) {
@@ -26,39 +37,38 @@ public class Main {
                     c = a * x + b;
                     str.append("найдите выражение: a*x+b ").append(a + "*X+" + b + "=" + c);
                     System.out.println(str);
-                    System.out.println("Правильный ответ:"+x);
                     break;
                 case 1:
                     //сконструировать expression
                     c = a * x * b;
                     str.append("найдите выражение: a*x*b ").append(a + "*X*" + b + "=" + c);
                     System.out.println(str);
-                    System.out.println("Правильный ответ:"+x);
+                    //System.out.println("Правильный ответ:"+x);
                     break;
                 case 2:
                     //сконструировать expression
                     c = a / x + b;
                     str.append("найдите выражение: a/x+b ").append(a + "/X+" + b + "=" + c);
                     System.out.println(str);
-                    System.out.println("Правильный ответ:"+x);
+                    //System.out.println("Правильный ответ:"+x);
                     break;
                 case 3:
                     //сконструировать expression
                     c = a / x - b;
                     str.append("найдите выражение: a/x-b ").append(a + "/X-" + b + "=" + c);
                     System.out.println(str);
-                    System.out.println("Правильный ответ:"+x);
+                    //System.out.println("Правильный ответ:"+x);
                     break;
                 case 4:
                     //сконструировать expression
                     c = a + x + b;
-                    System.out.println("Правильный ответ:"+x);
+                    //System.out.println("Правильный ответ:"+x);
                     str.append("найдите выражение: a+x+b ").append(a + "+X+" + b + "=" + c);
                     break;
                 case 5:
                     //сконструировать expression
                     c = a - x + b;
-                    System.out.println("Правильный ответ:"+x);
+                    //System.out.println("Правильный ответ:"+x);
                     str.append("найдите выражение: a-x+b ").append(a + "-X+" + b + "=" + c);
                     System.out.println(str);
                     break;
@@ -66,16 +76,33 @@ public class Main {
                 default:
                     ;
             }
+            System.out.println(x);
             System.out.println("Введите ответ:");
             int xx = scanner.nextInt();
-           // scanner.close();
             if (xx == x) {
                 System.out.println("True");
+                System.out.println("введите q - если хотите выйти, введите 1 - если хотите пример");
                 t += 1;
-            } else System.out.println("False");
-           // System.out.println("");
+            }
+            if (xx != x)
+                System.out.println("введите q - если хотите выйти, введите 1 - если хотите пример, 2- если хотите ввести еще один ответ");
+
+            String ab = scanner1.nextLine();
             str = new StringBuilder("");
+            switch (ab) {
+                case "q":
+                    s = ab;
+                    break;
+                case "1":
+                    ;
+                    continue;
+                case "2":
+                    System.out.println("Введите ответ еще раз");
+                    int xxx = scanner.nextInt();
+                    break;
+            }
+
         }
-        System.out.println("Правильных ответов:"+ t);
+        System.out.println("Правильных ответов:" + t);
     }
 }
