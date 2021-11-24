5a5afd9c4d51aad0e714d1e31df5708e95bba0d4
arebi <arebizov@yandex.ru>
2021-11-24 23:38:56 +0300

    add array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,42 @@ package com.company;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
+    public static class Equations {
+
+        String name;
+        int answer;
+        String timeS;
+        String timeE;
+        int x;
+
+        public Equations(String name, int answer, String timeS, String timeE, int x) {
+            this.name = name;
+            this.answer = answer;
+            this.timeS = timeS;
+            this.timeE = timeE;
+            this.x = x;
+        }
+    }
+
     public static void main(String[] args) {
+
+        List<Equations> list = new ArrayList<>();
 
         String expressionString1 = "";
         String expressionString2 = "";
-        //ArrayList<Example> cats = new ArrayList<Example>();
-        //ArrayList<String> arrayList= new ArrayList();
         Scanner scanner = new Scanner(System.in);
         Scanner scanner1 = new Scanner(System.in);
         StringBuilder str = new StringBuilder();
         System.out.println("Добрый день, введите своё имя:");
         String name = scanner.nextLine();
-        LocalDateTime time = LocalDateTime.now();
-        System.out.println(time);
+
+
         int t = 0;
         String s = "";
         System.out.println(s);
@@ -31,6 +49,7 @@ public class Main {
             int c;
 
             int expression = ThreadLocalRandom.current().nextInt(0, 5);
+            LocalDateTime timeS = LocalDateTime.now();
             switch (expression) {
                 case 0:
                     //сконструировать expression
@@ -76,9 +95,13 @@ public class Main {
                 default:
                     ;
             }
-            System.out.println(x);
+
+            //System.out.println(x);
+
             System.out.println("Введите ответ:");
             int xx = scanner.nextInt();
+            LocalDateTime timeE = LocalDateTime.now();
+            list.add(new Equations(str.toString(), xx, timeS.toString(), timeE.toString(), x));
             if (xx == x) {
                 System.out.println("True");
                 System.out.println("введите q - если хотите выйти, введите 1 - если хотите пример");
@@ -102,6 +125,10 @@ public class Main {
                     break;
             }
 
+        }
+        for (int i = 0; i < list.size(); i++) {
+            Equations equations = list.get(i);
+            System.out.println(equations.name + " Ответ: " + equations.answer + " Правильный ответ " + equations.x + " " + equations.timeS + " " + equations.timeE);
         }
         System.out.println("Правильных ответов:" + t);
     }
